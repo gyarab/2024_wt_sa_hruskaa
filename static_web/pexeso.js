2acad37f7aff148e1ed91830e418972afc959f0b
Adam Hruška <adam.hruska.s@gyarab.cz>
2024-12-11 11:39:58 +0100

    zmena pexeso

--- static_web/pexeso.js
+++ static_web/pexeso.js
@@ -6,10 +6,11 @@ let karty = [
             src: "https://placecats.com/millie/300/200"
         },
         {
-             src: "https://placecats.com/bella/300/200"
+            src: "https://placecats.com/bella/300/200"
         },
 ]
 
+/*
 let pole = [0,2,1,2,0,1]
 let el = document.getElementById('pexeso')
 pole.forEach((prvek) => {
@@ -18,7 +19,68 @@ pole.forEach((prvek) => {
     el.src = karty[prvek].src
     el.appendChild(newEl)
 })
+*/
 
-function show(){
+function show(cardid){
+    let cardElement = document.getElementById(cardid)
+    switch(n) {
+        case 1: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 2: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+            
+        case 3: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 4: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 5: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 6: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 7: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 8: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 9: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 10: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 11: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 12: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
     
+        case 13: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 14: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 15: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        case 16: 
+            cardElement.style.backgroundImage = "new_commission.jpg"
+
+        default :
+        console.log('neznama karta')
+        break;
+    
+
+    }
+
+    function showcard(karta, src) {
+        karta.style.backgroundImage = ''
+        karta
+    }
 }
